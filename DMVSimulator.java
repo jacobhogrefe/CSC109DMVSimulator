dd4ec115eacc15b6724fde1fb2d988a854e18319
jacobhogrefe <jhogrefe03@gmail.com>
2023-02-28 19:46:52 -0500

    set up DMV simulator

--- DMVSimulator.java
+++ DMVSimulator.java
@@ -1,3 +1,20 @@
+import java.util.Random;
+
 public class DMVSimulator {
-    
+    public static void main(String[] args) {
+        Random rand = new Random();
+        System.out.println("Welcome to the DMV! The happiest place on Earth!");
+        int userNumber = rand.ints(1, 1, 100).findFirst().getAsInt();
+        System.out.println("Your number is " + userNumber + ".");
+        int currentNumber = userNumber + 1;
+        while (currentNumber != userNumber) {
+            if (currentNumber == 100) {
+                currentNumber = 1;
+            }
+            System.out.println("Serving number " + currentNumber + ".");
+            currentNumber++;
+        }
+        System.out.println("Serving number " + userNumber + ".");
+        System.out.println("Sorry, you don't have the proper paperwork.");
+    }
 }
